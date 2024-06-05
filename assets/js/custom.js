8320134c23e572572840a91bbc4826448e230b8e
bashar0091 <awalbashar194@gmail.com>
2024-06-05 20:22:03 +0600

    fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,31 +2,34 @@ jQuery(document).ready(function($){
 
     // Function to close the popup form
     function closePopupForm() {
-        $('.popup_form_wrap').removeClass('active');
-        $('body').css('overflow','inherit');
-    }
+       $('.popup_form_wrap').removeClass('active');
+       $('body').css('overflow','auto');
+   }
 
-    // Open the popup form when the button is clicked
-    $('.email_popup_click').on('click', function(){
-        $('.popup_form_wrap').addClass('active');
-        $('body').css('overflow','hidden');
-    });
+   // Open the popup form when the button is clicked
+   $('.email_popup_click').on('click', function(){
+       $('.popup_form_wrap').addClass('active');
+       $('body').css('overflow','hidden');
+   });
 
-    // Close the popup form when clicking outside of it
-    $(document).on('click', function(event) {
-        if (!$(event.target).closest('.popup_form_wrap').length && !$(event.target).hasClass('email_popup_click')) {
-            closePopupForm();
-        }
-    });
+   // Close the popup form when clicking outside of it
+   $(document).on('click', function(event) {
+       if (!$(event.target).closest('.popup_form').length && !$(event.target).hasClass('email_popup_click')) {
+           closePopupForm();
+       }
+   });
 
-    // Prevent the form from closing when clicking inside it
-    $('.popup_form_wrap').on('click', function(event){
-        event.stopPropagation();
-    });
+   // Close the popup form when clicking the close button
+   $('.popup_close_btn').on('click', function(){
+       closePopupForm();
+   });
 
-    // Close the popup form when clicking the close button
-    $('.popup_close_btn').on('click', function(){
-        closePopupForm();
-    });
+   // get input value and show on field 
+   $('.on_input').on('input', function(){
+       var get_val = $(this).val();
+       var get_id = $(this).attr('id');
+
+       $(`.${get_id}`).text(get_val);
+   });
 
 });
